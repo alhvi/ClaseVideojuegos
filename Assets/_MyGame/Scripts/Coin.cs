87d96b7c0c76bd4f46ab89cad9493360a73ff10d
Alhvi Balcarcel <abalcarcel@gmail.com>
2024-02-06 19:43:08 -0600

    Ajustes
    Se agregó sonido. Se hizo que las fichas rotaran. Se agregó un game manager con patrón Singleton. Se actualizó el contador en pantalla de la cantidad de fichas colectadas.

--- Assets/_MyGame/Scripts/Coin.cs
+++ Assets/_MyGame/Scripts/Coin.cs
@@ -1,19 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Coin : MonoBehaviour
 {
-    
+
+    private float rotationSpeed = 40f;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void Update()
+    {
+        transform.Rotate(transform.up, rotationSpeed * Time.deltaTime);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("Ficha");
-            
-            Destroy(gameObject);
+            audioSource.PlayOneShot(audioSource.clip);
+            GameManager.Instance.GrabCoin();
+            StartCoroutine(DelayDestroy());
         }
+    }
+
+    private IEnumerator DelayDestroy()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Destroy(gameObject);
     }
 }
--- Assets/_MyGame/Scripts/GameManager.cs
+++ Assets/_MyGame/Scripts/GameManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -11,19 +9,62 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject coinBase;
 
-    void Start()
+
+    private static GameManager _instance;
+    private AudioSource audioSource;
+
+    public static GameManager Instance
     {
-        for (int i = 0; i < 10; i++)
-        { 
-            Vector3 randPos = Random.insideUnitSphere;
-            GameObject newObject = Instantiate(coinBase);
-            newObject.transform.position = randPos;
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new("GameManager");
+                _instance = go.AddComponent<GameManager>();
+            }
+            return _instance;
         }
     }
 
-    
-    void Update()
+
+
+    private void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
+
+
+    private void Start()
+    {
+        //Generando fichas en posiciones random
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 randPos = new(Random.Range(-10, 10), 0, Random.Range(0, 10));
+            GameObject newObject = Instantiate(coinBase);
+            newObject.transform.position = randPos;
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void GrabCoin()
+    {
+        count += 1;
+        m_TextMeshProUGUI.text = count.ToString();
+
+        if (count == 10)
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+
+    }
+
+
 }
